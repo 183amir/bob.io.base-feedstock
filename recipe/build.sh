d3157d0bb0910ea37d0e8cb3896c63ace5052ac4
Amir Mohammadi <183.amir@gmail.com>
2016-04-26 21:33:12 +0200

    Pull out of if. Remove C++11 boost variable.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ `uname` == 'Darwin' ]]; then
-	CXXFLAGS="${CXXFLAGS} -DBOOST_NO_CXX11_RVALUE_REFERENCES"
-	MACOSX_DEPLOYMENT_TARGET=10.9
-	$PYTHON -B setup.py install --single-version-externally-managed --record record.txt
-else
-	$PYTHON -B setup.py install --single-version-externally-managed --record record.txt
+	export MACOSX_DEPLOYMENT_TARGET=10.9
 fi
+
+$PYTHON -B setup.py install --single-version-externally-managed --record record.txt
